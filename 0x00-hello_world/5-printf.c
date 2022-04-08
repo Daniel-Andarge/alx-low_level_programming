b2176510890724cfc1280f8a372677151c378d12
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-08 01:31:45 -0700

    Task5.1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 include <studio.h>
-
+/**
+  *main - Entry point
+  *My hello, printf task
+  *Retun: return 1 after program execution
+  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
