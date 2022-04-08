06acded1d306d3119d940f0aaaab83f5f27e57f9
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-08 01:40:52 -0700

    Task5 Code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*My hello, printf task
-*Retun: return 1 after program execution
-*/
+ *main - Entry point
+ *My hello, printf task
+ *Retun: return 1 after program execution
+ */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return(0);
+{	
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return(0);
 }
