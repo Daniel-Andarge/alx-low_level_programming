41725fc2a6377cc02bf2a47f9d6ef17367da4778
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-08 06:57:28 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/Correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
