84c3b61af9018521f4293562f56f62b43f29a7c9
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-08 07:50:09 -0700

    Task -gerami

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,15 @@ char base_16;
 
 for (base_16 = 48; base_16 <=57; base_16++)
 {
-putchar( base_16 );
+putchar(base_16);
 }
 
 for (base_16 = 97 ; base_16 <=102; base_16++)
 {
-putchar( base_16 );
+putchar(base_16);
 }
 
-putchar ('\n');
+putchar('\n');
 
 return (0);
 
