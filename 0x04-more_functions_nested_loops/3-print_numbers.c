a5c5079a1fa2829a6f45aaa3f1cf0ae6b03d08e6
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-14 05:25:08 -0700

    Task-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i=0; i <=9; i++)
+{
+_putchar (i);
+i++;
+}
+
+_putchar ('\n');
+}
