0c579408a255d8c6c5e7188bc2b67f10640aed65
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-14 05:53:32 -0700

    Task-4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,21 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
+
 /**
- * print_numbers - print numbers
- * Return: Always 0.
+ * print_most_numbers - Print numbers between 0 to 9 incl.
+ *
+ * Return: Void.
  */
 void print_most_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i <= 9; i++)
-{
-if ((i == 2) || (i == 4))
-continue;
-else
-_putchar(i + '0');
-}
-_putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
 }
