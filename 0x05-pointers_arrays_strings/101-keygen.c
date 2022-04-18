d95c08c6e377eca6501e2792da2ca83eb5fa2962
Daniel-Andarge <andargedaniel90@gmail.com>
2022-04-18 05:45:40 -0700

    Task11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,39 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include <math.h>
+
 /**
+ * main - print password.
+ *
+ * Return: 0.
  */
+
 int main(void)
 {
-int num;
+	int ascii = 2772, i = 0, j, random;
+	char password[100];
+	time_t t;
 
-srand(time(0));
-num = rand();
-printf("%i\n", num);
-return (0);
+	srand((int) time(&t));
+	while (ascii > 126)
+	{
+		random = rand() % 126;
+		password[i] = random;
+		ascii -= random;
+		i++;
+	}
+	if (ascii > 0)
+		password[i] = ascii;
+	else
+	{
+		i--;
+	}
+	
+
+	for (j = 0; j <= i; j++)
+	{
+		printf("%c", password[j]);
+	}
+	return (0);
 }
