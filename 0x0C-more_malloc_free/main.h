438a7708a71c7638dd74b74786023e34e56d06b8
Daniel-Andarge <andargedaniel90@gmail.com>
2022-05-06 12:34:59 -0700

    mainPrototypr

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,10 +1,10 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-
+#include <limits.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -70,4 +70,4 @@ int _length(char *str)
 	return (i);
 }
 
-#endif /* HOLBERTON_H */
+#endif
