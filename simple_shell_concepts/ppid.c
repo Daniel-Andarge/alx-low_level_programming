457c2bf77adc154a0f69238e1205c595e4ad7027
Daniel-Andarge <andargedaniel90@gmail.com>
2022-06-07 02:25:46 -0700

    Parrent Process ID

---
+++ simple_shell_concepts/ppid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Parent PID
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	pid_t my_pid;
+	pid_t my_ppid;
+
+	my_pid = getpid();
+	printf("Process ID: %u\n", my_pid);
+	ppid = getppid();
+	printf("Parent Process ID: %u\n", my_ppid);
+	return (0);
+}
