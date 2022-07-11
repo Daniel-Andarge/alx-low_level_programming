c7dfe373c8488daae75a8c2b72cb099f3894b02c
Daniel-Andarge <andargedaniel90@gmail.com>
2022-07-11 13:27:54 -0700

    Calling C Functions from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int add(int x, int y)
+{
+	int sum = x+y;
+	return sum;
+}
+
+int multiply(int x, int y)
+{
+	int mul = x*y;
+	return mul;
+}
+
+int divide(int x, int y)
+{
+	int div =x/y;
+	return div;
+} 
+
+
+int subtruct(int x, int y)
+{
+	int sub =x-y;
+	return sub;
+} 
+
