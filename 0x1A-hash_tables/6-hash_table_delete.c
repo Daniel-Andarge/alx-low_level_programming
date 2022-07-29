4bc7cf462400ab2988103c1cc3457c35de8cb644
Daniel-Andarge <andargedaniel90@gmail.com>
2022-07-29 13:04:49 -0700

    a function that deletes hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,51 +1,31 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "hash_tables.h"
 
 /**
-  * hash_table_delete - Deletes a hash table
-  * @ht: The hash table to frees
-  *
-  * Return: Nothing!
-  */
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ *
+ * Return: void
+ */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i = 0;
-	hash_node_t *node = NULL, *temp = NULL;
+	unsigned long int i;
+	hash_node_t *next;
 
-	if (ht && ht->size && ht->array)
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+	for (i = 0; i < ht->size; i++)
 	{
-		for (; i < ht->size; ++i)
+		while (ht->array[i] != NULL)
 		{
-			node = ht->array[i];
-
-			if (node)
-			{
-				if (node->next)
-				{
-					node = node->next;
-					while (node)
-					{
-						temp = node;
-						node = node->next;
-						free(temp->key);
-						free(temp->value);
-						free(temp);
-					}
-				}
-
-				node = ht->array[i];
-				if (node->key && node->value)
-				{
-					free(node->key);
-					free(node->value);
-				}
-			}
-
-			free(node);
+			next = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = next;
 		}
-
-		free(ht->array);
-		free(ht);
 	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
